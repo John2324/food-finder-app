a8d60532f99d177eb36952bbbfca5f918da858d8
John <john@bytlabs.tech>
2020-04-01 10:03:01 -0700

    Add subcollection

--- app/src/main/java/com/google/firebase/example/yumtum/MainActivity.java
+++ app/src/main/java/com/google/firebase/example/yumtum/MainActivity.java
@@ -173,6 +173,9 @@ public class MainActivity extends AppCompatActivity implements
     private void onAddItemsClicked() {
         // Get a reference to the restaurants collection
         CollectionReference restaurants = mFirestore.collection("restaurants");
+        CollectionReference subRef = mFirestore.collection("restaurants")
+                .document("abc123")
+                .collection("ratings");
 
         for (int i = 0; i < 10; i++) {
             // Get a random Restaurant POJO
@@ -185,8 +188,35 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onFilter(Filters filters) {
-        // TODO(developer): Construct new query
-        showTodoToast();
+        // Construct query basic query
+        Query query = mFirestore.collection("restaurants");
+
+        // Category (equality filter)
+        if (filters.hasCategory()) {
+            query = query.whereEqualTo("category", filters.getCategory());
+        }
+
+        // City (equality filter)
+        if (filters.hasCity()) {
+            query = query.whereEqualTo("city", filters.getCity());
+        }
+
+        // Price (equality filter)
+        if (filters.hasPrice()) {
+            query = query.whereEqualTo("price", filters.getPrice());
+        }
+
+        // Sort by (orderBy with direction)
+        if (filters.hasSortBy()) {
+            query = query.orderBy(filters.getSortBy(), filters.getSortDirection());
+        }
+
+        // Limit items
+        query = query.limit(LIMIT);
+
+        // Update the query
+        mQuery = query;
+        mAdapter.setQuery(query);
 
         // Set header
         mCurrentSearchView.setText(Html.fromHtml(filters.getSearchDescription(this)));
